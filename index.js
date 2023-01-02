f741a8f4251ae05338fe25f8f59a6e45e67765d9
Raneem <aljamal.raneem@gmail.com>
2023-01-02 23:55:03 +0200

    step 4

--- index.js
+++ index.js
@@ -19,9 +19,38 @@ let minutes = date_ob.getMinutes();
 let seconds = date_ob.getSeconds();
 
 app.get('/time', (req, res) => {
-    res.send(hours + ":" + minutes + ":" + seconds)
+    res.send({status: 200, message: hours + ":" + minutes + ":" + seconds})
   })
-  
+// hello id command
+app.get('/hello/:id', (req, res) => {
+    const id = req.params.id;
+    res.send({ status: 200, message: `Hello,${id}` });
+  });  
+  app.get('/hello', (req, res) => {
+    const id = req.params.id;
+    res.send({ status: 200, message: `Hello` });
+  });  
+// search  command
+app.get('/search', (req, res) => {
+  const search = req.query.s;
+  if (search) {
+    res.status(200).json({
+      status: 200,
+      message: "ok",
+      data: search
+    });
+  } else {
+    res.status(500).json({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+  }
+});
+
+
+
+
   app.listen(port, () => {
     console.log(`Example app listening on ${port}`)
   })
