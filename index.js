2c3e392754d8065aafbe6fe1c6bdf93984aaa9ae
Raneem <aljamal.raneem@gmail.com>
2023-01-02 22:33:38 +0200

    step 2

---
+++ index.js
@@ -0,0 +1,13 @@
+//create a server
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('OK')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
