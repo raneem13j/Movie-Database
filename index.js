7495d0ac674a758cd61901a4bf4f5ba24976d7d9
Raneem <aljamal.raneem@gmail.com>
2023-01-02 23:05:06 +0200

    step 3

--- index.js
+++ index.js
@@ -3,11 +3,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('OK')
+// test command
+app.get('/test', (req, res) => {
+  res.send("OK")
 })
+// time command
+let date_ob = new Date();
+// current hours
+let hours = date_ob.getHours();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// current minutes
+let minutes = date_ob.getMinutes();
+
+// current seconds
+let seconds = date_ob.getSeconds();
+
+app.get('/time', (req, res) => {
+    res.send(hours + ":" + minutes + ":" + seconds)
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on ${port}`)
+  })
